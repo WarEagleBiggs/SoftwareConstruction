90238c30f67856a58e2a322e35af7066e222efbb
Biggs <48188031+WarEagleBiggs@users.noreply.github.com>
2023-08-19 11:12:40 -0500

    comments

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,19 +1,34 @@
+//AUTHOR: Adam Biggs
+//Project 1 - Stereo System Payments
 #include <iostream>
 using namespace std;
 
-//vars
-
-
 //main
 int main() {
-    string loanAmount;
 
+    //vars
+    int loanAmount;
+    int monthlyPayment;
+    int interestRate;
+
+    //---------------Loan Amount------------------------
     cout << "***********************" << endl;
     cout << "*  Enter Loan Amount  *" << endl;
     cout << "***********************" << endl;
     cin >> loanAmount;
-    cout << "* Confirm loan of " + loanAmount + " *" << endl;
-    cout << "*         Y/N         *" << endl;
+
+    //check if loanAmount is negative
+    if (loanAmount <= 0){
+        //error
+        cout << "Please enter a number greater than 0" << endl;
+        cin >> loanAmount;
+    }
+    //---------------Monthly Payment--------------------
+
+
+    //---------------Interest Rate----------------------
+
+
 
 
     return 0;
